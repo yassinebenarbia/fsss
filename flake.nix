83d014e63efe71e64e5597583e44a67053e496ea
yassinebenarbia <yassine.bna@proton.me>
2026-06-27 21:59:01 +0200

    chor: update flake file

--- flake.nix
+++ flake.nix
@@ -1,19 +1,44 @@
 {
-    description = "";
-    inputs = {
-        nixpkgs.url = "nixpkgs:NixOS/nixos/stable";
-    };
-    outputs = {self, nixpkgs, inputs, ...}:
-    let 
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
+  description = "Flake file for the FSSServer";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
     in
     {
-        pkgs.mkShell {
-            buildINputs = [];
-            nativeBuildInputs= [];
-            shellHook = ``echo "HIII"``;
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          openssl
+          pkg-config
+        ];
+        nativeBuildInputs = with pkgs; [
+          postgresql
+          websocat
+          redis
+          pgcli
+        ];
+        shellHook = ''echo "Welcome!"'';
+      };
 
-        }
+      defaultPackage.x86_64-linux = pkgs.rustPlatform.buildRustPackage {
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+        buildInputs = with pkgs; [
+          openssl
+        ];
+        pname = "fsss";
+        src = ./.;
+        version = "0.1.0";
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+      };
     };
 }
